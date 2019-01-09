42b04453ab5cd661bb06bdbacdabc57aec219963
Worm <Worm@worms-air.home>
2019-01-09 04:12:21 +0100

    init

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Off canvas navigation*/
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
 
 
 function myFunction(x) {
-  if (x.matches) { // If media query matches
-
-document.querySelector("#mySidenav").classList.remove("hide");
-      document.querySelector("#openbtn").classList.remove("hide");
-  }
+    if (x.matches) { // If media query matches
+        document.querySelector("#mySidenav").classList.remove("hide");
+        document.querySelector("#openbtn").classList.remove("hide");
+    }
 }
 
-var x = window.matchMedia("(max-width: 400px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+var x = window.matchMedia("(max-width: 400px)");
+myFunction(x)
+x.addListener(myFunction);
+
+
+
+/*Slideshow*/
+var slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 8000); // Change image every 10 seconds
+}
